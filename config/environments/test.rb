380a57149ce47912ccfcaad9427e2963a208d079
Nicolas Dabreo <nick.dabreo@hotmail.com>
2020-12-03 21:29:18 +0000

    Rubocop

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -46,4 +48,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Set factory_bot to use custom dir
+  config.factory_bot.definition_file_paths = ['support/factories']
 end
